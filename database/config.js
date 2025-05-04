9c8267f26e36ecfe8df17d5c513515569813f57b
erick <paca.systems.ventas@gmail.com>
2025-05-04 09:58:50 -0500

    probando otra solucion al error de coneccion a DB

--- database/config.js
+++ database/config.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const mongoDB = require('../config/mongo_url');
 
+console.log(mongoDB.url);
+
 const dbConnection = async() => {
 
     try{
@@ -13,6 +15,7 @@ const dbConnection = async() => {
         });
 
         console.log('Base de datos online');
+        
 
     }catch(error){
         console.log(error);
